985c2c010152cc4c90904e855695ec889b3cd83b
Manuel Pollhammer <109719423+pollhammer@users.noreply.github.com>
2026-07-01 14:57:59 +0200

    Fix title formatting in system settings page

--- packages/frontend/src/routes/dashboard/settings/system/+page.svelte
+++ packages/frontend/src/routes/dashboard/settings/system/+page.svelte
@@ -55,7 +55,7 @@
 </script>
 
 <svelte:head>
-	<title>{$t('app.system_settings.title')} -  Pollhammer Mail Archive</title>
+	<title>{$t('app.system_settings.title')} - Pollhammer Mail Archive</title>
 </svelte:head>
 
 <div class="space-y-6">
